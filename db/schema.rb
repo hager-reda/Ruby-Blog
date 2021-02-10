280d74ec84b182d98b5a3a44b79555886f942419
hager <hgrreda@gmail.com>
2021-02-10 21:26:19 +0200

    added comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_121024) do
+ActiveRecord::Schema.define(version: 2021_02_10_172516) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
